fa4ba86e2fce3f0974735c17d1d8290606c56b9d
Stefan Samba <stefan@flow.ai>
2022-08-11 10:11:33 +0200

    added visuals

--- job.py
+++ job.py
@@ -1,18 +1,25 @@
+import dagstermill as dm
 from dagster import In, Out, job
 from dagster.utils import script_relative_path
 
-import dagstermill as dm
-
 load_data = dm.define_dagstermill_op(
+    # Name of the op:
     "load_data",
+    # Path to the notebook
     notebook_path=script_relative_path("load_data.ipynb"),
+    # The op's outputs.
     outs={"numbers": Out(list, description="list of numbers")}
 )
 
+
 read_data = dm.define_dagstermill_op(
+    # Name of the op:
     "read_data",
+    # Path to the notebook
     notebook_path=script_relative_path("read_data.ipynb"),
-    ins={"numbers": In(list, description="list of numbers obtained from load_data.ipynb")}
+    # The op's inputs.
+    ins={"numbers": In(list, description="list of numbers obtained from\
+                                          load_data.ipynb")}
 )
 
 
